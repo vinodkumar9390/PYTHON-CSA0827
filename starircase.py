c0f3e6ebb2acf7b7a88100acd41a1abdb2b38419
vinodkumar9390 <114823478+vinodkumar9390@users.noreply.github.com>
2024-03-20 10:44:41 +0530

    Add files via upload

---
+++ starircase.py
@@ -0,0 +1,11 @@
+def distnict(n):
+    dp=[]
+    dp.append(1)
+    dp.append(1)
+    for i in range(2,n+1):
+        dp[i-1]+dp[i-2]
+        dp.append(dp[i-1]+dp[i-2])
+    return dp
+n=int(input("enter the number of steps:"))
+dist=distnict(n)
+print(dist[n])
